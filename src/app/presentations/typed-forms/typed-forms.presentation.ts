8f7b1dd3b147a79aeb6c87c89b2304a64e7b5186
ljclaeyssen <ljclaeyssen@gmail.com>
2025-10-27 14:56:51 +0100

    feat: presentation

--- src/app/presentations/typed-forms/typed-forms.presentation.ts
+++ src/app/presentations/typed-forms/typed-forms.presentation.ts
@@ -1,6 +1,8 @@
 import { Component, ChangeDetectionStrategy, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { register } from 'swiper/element/bundle';
 import { IntroSlide } from './slides/intro.slide';
+import { FormBuilderProblemSlide } from './slides/formbuilder-problem.slide';
+import { AdaptiveCodeProblemSlide } from './slides/adaptive-code-problem.slide';
 
 register();
 
@@ -8,7 +10,7 @@ register();
   selector: 'app-typed-forms-presentation',
   templateUrl: './typed-forms.presentation.html',
   styleUrl: './typed-forms.presentation.scss',
-  imports: [IntroSlide],
+  imports: [IntroSlide, FormBuilderProblemSlide, AdaptiveCodeProblemSlide],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
