317b47f2e6a52e9a2968f5c476cc05da93745526
ljclaeyssen <ljclaeyssen@gmail.com>
2025-10-27 18:32:12 +0100

    feat: toaster navigation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
+import { Toast } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Toast],
+  providers: [MessageService],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {}
+export class App implements OnInit {
+  constructor(
+    private router: Router,
+    private messageService: MessageService
+  ) {}
+
+  ngOnInit() {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event) => {
+        const url = (event as NavigationEnd).urlAfterRedirects;
+        if (url !== '/') {
+          this.messageService.add({
+            severity: 'info',
+            summary: 'Navigation',
+            detail: 'Pour naviguer dans la présentation, utilisez les flèches du clavier, cliquez sur les flèches latérales ou bien swipez !',
+            life: 5000
+          });
+        }
+      });
+  }
+}
